fa9cc1b1cfb65c3f5e850567f2e864939a99799e
Alejogb1 <alejogarciag1@gmail.com>
2021-02-09 10:44:39 -0300

    Update positiveAverage.js
    Co-Authored-By: Alejo Garcia <52103963+alejogb1@users.noreply.github.com>

--- part1/unicafe/src/components/positiveAverage.js
+++ part1/unicafe/src/components/positiveAverage.js
@@ -1,6 +1,7 @@
-const positiveAverage = (postive, total) => {
-    const result = positive/total
-    return(<p>{result}</p>)
+const positiveAverage = ({positive, total}) => {
+    let result = positive/total
+    result = Math.trunc(result*100) 
+    return(<p>{result}%</p>)
   }
   
   export default positiveAverage
