7f433cef13a61fa3dee4cd54b0fdf6caa7faf529
Alejogb1 <alejogarciag1@gmail.com>
2021-02-12 18:47:21 -0300

    Update index.js
    Co-Authored-By: Alejo Garcia <52103963+alejogb1@users.noreply.github.com>

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import Average from "./components/Average"
 import PositiveAverage from "./components/PositiveAverage"
+import Button from "./components/Button"
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [clicks, setClicks] = useState(0)
+  const [selected, setSelected] = useState(0)
   const handleGood = () => {
      setClicks(clicks +1)
      setGood(good +1)
@@ -20,23 +22,54 @@ const App = () => {
     setClicks(clicks +1)
     setNeutral(neutral +1)
   }
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+  const handlerQuote = () => {
+    setSelected(+1)
+  }
   if (clicks === 0) {
     return (
       <div>
-        <button onClick={handleGood}>Good</button>
-        <button onClick={handleBad}>Bad</button>
-        <button onClick={handleNeutral}>Neutral</button>
-        <p>Good: {good} | Bad:  {bad} | Neutral: {neutral} | All: {clicks} </p>
-         <p>No given feedback</p>
+        <Button handleBad={handleBad} handleGood={handleGood} handleNeutral={handleNeutral}/>
+        <button onClick={handlerQuote}>Random swe qoute</button>
+        {anecdotes[selected]}
+        <div style={{marginTop: "50px"}}>
+          <tr> Good: 
+            <td>{good}</td>
+          </tr>
+          <tr> Negative: 
+            <td>{bad}</td>
+          </tr>
+          <tr> Neutral: 
+            <td>{neutral}</td>
+          </tr>
+        </div>
+        <p>No given feedback</p>
       </div>
     )
   } else {
     return (
       <div>
-        <button onClick={handleGood}>Good</button>
-        <button onClick={handleBad}>Bad</button>
-        <button onClick={handleNeutral}>Neutral</button>
-        <p>Good: {good} | Bad:  {bad} | Neutral: {neutral} | All: {clicks} </p>
+        <Button  handleBad={handleBad} handleGood={handleGood} handleNeutral={handleNeutral}/>
+        <button onClick={handlerQuote}>Random swe qoute</button>
+        {anecdotes[selected]}
+        <div style={{marginTop: "50px"}}>
+          <tr> Good: 
+            <td>{good}</td>
+          </tr>
+          <tr> Negative: 
+            <td>{bad}</td>
+          </tr>
+          <tr> Neutral: 
+            <td>{neutral}</td>
+          </tr>
+        </div>
         <Average positive={good} total={clicks} negative={bad}/>
            <PositiveAverage positive={good} total={clicks}/>
       </div>
@@ -44,6 +77,7 @@ const App = () => {
   }
   
 }
+
 
 ReactDOM.render(<App />, 
   document.getElementById('root')
