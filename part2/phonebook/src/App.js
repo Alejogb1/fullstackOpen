3290bb9f3234cde24f5ee6a84e48dcb99f83d135
Alejogb1 <52103963+Alejogb1@users.noreply.github.com>
2021-03-14 10:29:24 -0300

    Update App.js

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,33 +2,61 @@ import React, { useState } from 'react'
 
 const App = () => {
 
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+
+  const [ persons, setPersons ] = useState([]) 
 
   const [ newName, setNewName ] = useState('')
 
+  const [ repeated, setRepeated] = useState(false)
+
   const handlerName = (e) => {
+    console.log(e.target.value)
+     
     setNewName(e.target.value) 
   } 
   
-  const submitName = (e) => {
+
+
+  const nameFilter = persons.filter(person => person.name !== newName)
+  const addNote = (e) => {
     e.preventDefault()
-    setPersons(persons, [setPersons])
+    const noteObject = {
+      name : newName,
+    } 
+
+    
+    /*
+    for(let i = 0; i < persons.length +1; ) {
+      console.log("person number", i, persons[i])
+      if (persons[i].name === noteObject.name) {
+        alert("repetido")
+      } else {
+        setPersons(persons.concat(noteObject))
+      }
+      i++
+    }*/
+    console.log("lol")
+    setNewName("")
   }
 
+  const notesToShow = () => {
+    return 
+  }
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={submitName}>
+      <form onSubmit={addNote}>
         <div>
-          name: <input onChange={handlerName} />
+          name: <input onChange={handlerName} value={newName} />
         </div>
         <div>
-          <button type="submit" value="Submit">add</button>
+          <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      {
+        nameFilter.map( person => <h4>{person.name}</h4>)
+      }
     </div>
   )
 }
