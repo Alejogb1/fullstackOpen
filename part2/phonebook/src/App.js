4e35f5d47b6d596912fcae8e8016ed772d11e835
Alejogb1 <52103963+Alejogb1@users.noreply.github.com>
2021-03-19 13:09:04 -0300

    PersonForm 🦴

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,61 +1,59 @@
 import React, { useState } from 'react'
-
+import PersonForm from "./components/personForm"
 const App = () => {
 
 
-  const [ persons, setPersons ] = useState([]) 
+  const [ persons, setPersons ] = useState([{name: "Juan", number: "94234242"}]) 
 
   const [ newName, setNewName ] = useState('')
 
-  const [ repeated, setRepeated] = useState(false)
+  const [ newNumber, setNewNumber ] = useState(0)
 
-  const handlerName = (e) => {
-    console.log(e.target.value)
-     
+
+  const handleChangeName = (e) => {
     setNewName(e.target.value) 
   } 
-  
+  const handleChangeNumber = (e) => {
+    setNewNumber(e.target.value) 
+  } 
 
-
-  const nameFilter = persons.filter(person => person.name !== newName)
   const addNote = (e) => {
     e.preventDefault()
     const noteObject = {
       name : newName,
+      number : newNumber
     } 
 
+    console.log("lol")
+    setPersons(persons.filter(person => person.name !== newName).concat(noteObject)) 
     
-    /*
+  /*  
     for(let i = 0; i < persons.length +1; ) {
       console.log("person number", i, persons[i])
       if (persons[i].name === noteObject.name) {
-        alert("repetido")
-      } else {
-        setPersons(persons.concat(noteObject))
+        alert(`${newName} is already added to phonebook`)
       }
       i++
     }*/
-    console.log("lol")
     setNewName("")
   }
 
-  const notesToShow = () => {
-    return 
-  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addNote}>
-        <div>
-          name: <input onChange={handlerName} value={newName} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
+      <PersonForm addNote={addNote} newName={newName} newNumber={newNumber} handleChangeName={handleChangeName} handleChangeNumber={handleChangeNumber}/>
+   
+      <h2>Names</h2>
       {
-        nameFilter.map( person => <h4>{person.name}</h4>)
+        persons.map( person => 
+        
+          <div className="container">
+            <h4>New Note</h4>
+            <p>{person.name}</p>
+            <p>{person.number}</p>
+          </div>
+        )
       }
     </div>
   )
